fc2c22cb63aad731d00a802e2fbe94a9d6a96fff
EgorovM <egorov_michil@mail.ru>
2018-11-18 11:50:47 +0900

    c

--- C.py
+++ C.py
@@ -7,13 +7,12 @@ Q = 0
 for i in range(N):
     a = list(map(int,fin.readline().split()))
 
-    if A >= a[1]:
-        if a[2] - 2*a[0]*S > -a[0]*S:
-            A += a[2]
-            Q += 2*a[0]*S
-
-        else:
-            Q += a[0]*S
+    
+    if a[2] - 2*a[0]*S > -a[0]*S and A >= a[1]:
+        A += a[2]
+        Q += 2*a[0]*S
+    else:
+        Q += a[0]*S
 
 fout.write(str(A-Q))
 
