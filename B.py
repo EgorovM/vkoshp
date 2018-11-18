e673510c3a0caf805a0573cbdc27bbf1005dd539
EgorovM <egorov_michil@mail.ru>
2018-11-18 11:54:52 +0900

    b

--- B.py
+++ B.py
@@ -3,7 +3,9 @@ fout = open("output.txt","w")
 
 n = int(fin.readline())
 
-if n % 2 == 0:
+if n == 2:
+    fout.write("2")
+elif n % 2 == 0:
     fout.write(str(n//2))
 elif n % 3 == 0:
     fout.write(str(n//3 * 2))
