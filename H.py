94ce95597fb4d6e4b3be24f666ea1cf649e1e217
EgorovM <egorov_michil@mail.ru>
2018-11-18 15:24:29 +0900

    h

--- H.py
+++ H.py
@@ -20,6 +20,8 @@ for i in range(m):
     sh[b].append(a)
 
     gl[a].append(b)
+    
+win = None
 
 if len(winner) - sum(winner) > 1:
     visit = [0 for i in range(n)]
@@ -44,36 +46,57 @@ if len(winner) - sum(winner) > 1:
                 k += 1
             kp.append(q)    
 
-
-
-
     if len(kp) > 1:
         fout.write("No")
     else:
         visit = [0 for i in range(n)]
         l = []
         
-        
-        stek = [win]
-        visit[win-1] = 1
+        if win != None:
+            stek = [win]
+            visit[win-1] = 1
 
-        while len(stek) > 0:
-            fl = False
-            for i in range(len(gl[stek[-1]])):
-                if visit[gl[stek[-1]][i]-1] == 0:
-                    l.append([stek[-1],gl[stek[-1]][i]])
-                    visit[gl[stek[-1]][i]-1] = 1
-                    stek.append(gl[stek[-1]][i])
-                    fl = True
-                    
-                    break
+            while len(stek) > 0:
+                fl = False
+                for i in range(len(gl[stek[-1]])):
+                    if visit[gl[stek[-1]][i]-1] == 0:
+                        l.append([stek[-1],gl[stek[-1]][i]])
+                        visit[gl[stek[-1]][i]-1] = 1
+                        stek.append(gl[stek[-1]][i])
+                        fl = True
+                        
+                        break
 
-            if fl == False:
-                stek.pop()
-                    
-        fout.write("Yes\n")
-        for i in range(len(l)):
-            fout.write(str(l[-1-i][0]) + " " + str(l[-1-i][1]) + "\n")
+                if fl == False:
+                    stek.pop()
+                        
+            fout.write("Yes\n")
+            for i in range(len(l)):
+                fout.write(str(l[-1-i][0]) + " " + str(l[-1-i][1]) + "\n")
+                
+        else:
+            for st in range(1,n+1):
+                stek = [st]
+                visit[st-1] = 1
+                
+                while len(stek) > 0:
+                    fl = False
+                    for i in range(len(gl[stek[-1]])):
+                        if visit[gl[stek[-1]][i]-1] == 0:
+                            l.append([stek[-1],gl[stek[-1]][i]])
+                            visit[gl[stek[-1]][i]-1] = 1
+                            stek.append(gl[stek[-1]][i])
+                            fl = True
+                            
+                            break
+
+                    if fl == False:
+                        stek.pop()
+                        
+            fout.write("Yes\n")
+            
+            for i in range(len(l)):
+                fout.write(str(l[-1-i][0]) + " " + str(l[-1-i][1]) + "\n")
 else:
     fout.write("No")
 
